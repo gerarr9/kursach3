e97c8c7eeda6aa3b4398cc3c361967145ba41827
321ru <gerarr999@yandex.ru>
2023-02-03 13:10:32 +0300

    main

---
+++ src/main/java/com/example/kursach33/Kursach33Application.java
@@ -0,0 +1,13 @@
+package com.example.kursach33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursach33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursach33Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursach33/Kursach33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursach33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursach33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
